a588d86164b642de47981efb859e6ec56a757faf
satadok <tadokoro@rm.is.tohoku.ac.jp>
2017-02-05 02:42:57 -0500

    The first test commit

---
+++ ungarble.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# ungarble.rb
+#
+# Function
+#    Correct name of a garbled file in Macintosh.
+#    Names of downloaded files are garbled as follows:
+#    - A multi-byte character in RTF-8 --> "%"+hex2digits + "%"+...
+#    - A single-byte character --> no change
+#    - Example: ご利_2015.csv --> %e3%81%94%e5%88%a9_2015.csv
+#
+# Input/Output
+#    $stdin: garbled_filename
+#    $stdout: ungarbled_filename
+#
+while fname = gets do
+  if fname == nil then
+    break
+  else
+    fname = fname.chomp
+    String ugfname = fname.gsub(/%([0-9a-fA-F][0-9a-fA-F])/){$1.hex.chr}
+    if fname != ugfname then
+      com = "mv #{fname} #{ugfname}"
+      result = system(com)
+      puts result
+    end
+  end
+end
